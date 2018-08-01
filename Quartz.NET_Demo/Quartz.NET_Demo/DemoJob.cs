1d23e0494517fbffa6b87a4b45f9f610117adcdf
NaylorLeading <cnaylor@163.com>
2018-08-01 18:01:18 +0800

    添加了Quartz源代码3.X版本；添加了基于2.x版本写的一个Demojob

---
+++ Quartz.NET_Demo/Quartz.NET_Demo/DemoJob.cs
@@ -0,0 +1,31 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartz.NET_Demo
+{
+    public class DemoJob : IJob
+    {
+        /// <summary>
+        /// 3.0之前版本写法
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        //public async Task Execute(IJobExecutionContext context)
+        //{
+        //    await Console.Out.WriteLineAsync("DemoJob is executing.");
+        //}
+
+
+
+
+        //2.0
+        void IJob.Execute(IJobExecutionContext context)
+        {
+            Console.Out.WriteLineAsync("DemoJob is executing.");
+        }
+    }
+}
--- Quartz.NET_Demo/Quartz.NET_Demo/Program.cs
+++ Quartz.NET_Demo/Quartz.NET_Demo/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Quartz.Impl;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +12,58 @@ namespace Quartz.NET_Demo
     {
         static void Main(string[] args)
         {
+            Start();
+        }
 
-        }
-        public void test()
+
+        /// <summary>
+        /// 3.0之后版本的写法
+        /// </summary>
+        /// <returns></returns>
+        public async Task Do()
         {
-            int i = 0;        
+            //        NameValueCollection props = new NameValueCollection
+            //{
+            //    { "quartz.serializer.type", "binary" }
+            //};
+            //        StdSchedulerFactory factory = new StdSchedulerFactory(props);
+
+            //        IScheduler sched = await factory.GetScheduler();
+            //        await sched.Start();
+
+            //        IJobDetail job = JobBuilder.Create<DemoJob>()
+            //            .WithIdentity("myJob", "group1")
+            //            .Build();
+
+            //        ITrigger trigger = TriggerBuilder.Create()
+            //            .WithIdentity("myTrigger", "group1")
+            //            .StartNow()
+            //            .WithSimpleSchedule(x => x
+            //                .WithIntervalInSeconds(40)
+            //                .RepeatForever())
+            //        .Build();
+            //        await sched.ScheduleJob(job, trigger);
         }
+
+        /// <summary>
+        /// 2.0
+        /// </summary>
+        public static void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+            IJobDetail job = JobBuilder.Create<DemoJob>().Build();
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("IDGJob", "IDG")
+                .StartNow()
+                .WithSimpleSchedule(s => s.WithIntervalInSeconds(3).RepeatForever())
+                .Build();
+            scheduler.ScheduleJob(job, trigger);
+        }
+
     }
+
+
 
 
 }
